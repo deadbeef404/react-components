36facd24da1f382288c03c8508446ea7ee69858b
Jason Johnson <jjohnson@dataminr.com>
2015-06-10 16:25:58 -0600

    Simplified guid function in utils.

--- src/js/utils/Utils.js
+++ src/js/utils/Utils.js
@@ -6,17 +6,12 @@ define(function(require) {
     var React = require('react');
 
     var Utils = {
-        guid: (function() {
-            function s4() {
-                return Math.floor((1 + Math.random()) * 0x10000)
-                    .toString(16)
-                    .substring(1);
-            }
-            return function() {
-                return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-                    s4() + '-' + s4() + s4() + s4();
+        guid: function() {
+            var s4 = function() {
+                return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
             };
-        })(),
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+        },
 
         /**
          * Get formatted time string given the start and end times.
