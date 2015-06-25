9a635063a93fbc65ec23978a8a7c205de014eabd
Jason Johnson <jjohnson@dataminr.com>
2015-06-25 16:30:19 -0600

    Commented PageMessage.

--- src/js/utils/PageMessage.js
+++ src/js/utils/PageMessage.js
@@ -28,12 +28,14 @@ define(function(require) {
         },
 
         componentDidMount: function() {
+            // Dismiss the message with animation after the duration time if the message was not already closed manually.
             this.timeout = setTimeout(function() {
                 this.dismiss(true);
             }.bind(this), this.props.duration);
         },
 
         componentWillUnmount: function() {
+            // Future messages would have incorrect duration times if this is not cleared.
             clearTimeout(this.timeout);
             this.timeout = null;
         },
@@ -50,10 +52,15 @@ define(function(require) {
             );
         },
 
+        /**
+         * Builds the markup for the message that is to be displayed.
+         * @returns {ReactElement|Null} - The element containing the message.
+         */
         getMessageMarkup: function() {
             var closeIcon = this.props.closeIcon ? <i className={this.props.closeIcon + ' close'} onClick={this.dismiss.bind(this, false)} /> : null;
             var icon = this.props.icon ? <i className={this.props.icon} /> : null;
 
+            // When null is returned, the message will remain in the DOM until the animation has completed if an animation was requested.
             if (this.state.leaving) {
                 return null;
             }
@@ -66,13 +73,20 @@ define(function(require) {
             );
         },
 
+        /**
+         * Triggers dismissal of the page message with or without animation.
+         * @param {Number} animate - The duration to wait before animation.
+         */
         dismiss: function(animate) {
+            // If the close button was clicked, don't animate the message off of the screen. Simply remove it immediately.
             var animationAllowance = animate ? 1000 : 0;
 
+            // Triggers removal of the message for the ReactCSSTransitionGroup
             this.setState({
                 leaving: animate
             });
 
+            // Delay the closing of the portal so that the animation is allowed to complete.
             this.timeout = setTimeout(function() {
                 PortalMixins.closePortal();
             }, animationAllowance);
